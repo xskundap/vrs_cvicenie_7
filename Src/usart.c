dc016e84375cb295941c08a966b70a887b0f9a1d
Tvarožek Marian <91201706+xtvarozekm@users.noreply.github.com>
2021-11-18 16:53:44 +0100

    Update usart.c

--- Src/usart.c
+++ Src/usart.c
@@ -24,8 +24,6 @@
 uint8_t bufferUSART2dma[DMA_USART2_BUFFER_SIZE];
 
 /* Declaration and initialization of callback function */
-//static void (* USART2_ProcessData)(uint8_t data) = 0;
-//static void (* USART2_ProcessData)(const uint8_t* data) = 0;
 static void (* USART2_ProcessData)(const uint8_t* data, uint16_t len) = 0;
 /* Register callback */
 void USART2_RegisterCallback(void *callback)
@@ -166,43 +164,8 @@ void USART2_PutBuffer(uint8_t *buffer, uint8_t length)
  *	Keeps track of pointer pointing to Rx memory buffer and resets the pointer if overflow is possible in next Rx.
  *	Refer to reference manual - "normal memory mode" and "increment memory mode".
  */
-/*
-void USART2_CheckDmaReception(void)
-{
-	//type your implementation here
-	if(USART2_ProcessData == 0) return;
-	
-	static uint16_t old_pos = 0;
 
-	uint16_t pos = DMA_USART2_BUFFER_SIZE - LL_DMA_GetDataLength(DMA1, LL_DMA_CHANNEL_6);
 
-		if (pos != old_pos)
-		{
-			if (pos > old_pos)
-			{
-				USART2_ProcessData(&bufferUSART2dma[old_pos]);
-			}
-			else
-			{
-				USART2_ProcessData(&bufferUSART2dma[old_pos]);
-
-				if (pos > 0)
-				{
-					USART2_ProcessData(&bufferUSART2dma[0]);
-				}
-			}
-		}
-
-		old_pos = pos;
-
-		if (old_pos == DMA_USART2_BUFFER_SIZE)
-		{
-			old_pos = 0;
-		}
-	
-}
-*/
-/*
 void USART2_CheckDmaReception(void)
 {
 	if(USART2_ProcessData == 0) return;
@@ -215,63 +178,14 @@ void USART2_CheckDmaReception(void)
 
 	if (pos != old_pos)
 	{
-		if (pos > old_pos)
-		{
-			USART2_ProcessData(&bufferUSART2dma[old_pos], pos - old_pos);
-		}
-		else
-		{
-			USART2_ProcessData(&bufferUSART2dma[old_pos], DMA_USART2_BUFFER_SIZE - old_pos);
-
-			if (pos > 0)
-			{
-				USART2_ProcessData(&bufferUSART2dma[0], pos);
-			}
-		}
-	}
-
-	old_pos = pos;
-
-	if (old_pos == DMA_USART2_BUFFER_SIZE)
-	{
-		old_pos = 0;
-	}
-}
-*/
-void USART2_CheckDmaReception(void)
-{
-	if(USART2_ProcessData == 0) return;
-
-	static uint16_t old_pos = 0;
-
-	uint16_t pos = DMA_USART2_BUFFER_SIZE - LL_DMA_GetDataLength(DMA1, LL_DMA_CHANNEL_6);
-
-	occupiedMem = pos;
-
-	if (pos != old_pos)
-	{
-		//if (pos > old_pos)
 		if (pos <= DMA_USART2_BUFFER_SIZE-1)
 		{
 			USART2_ProcessData(&bufferUSART2dma[old_pos], pos - old_pos);
-			//USART2_ProcessData(&bufferUSART2dma[old_pos]);
 			old_pos = pos;
 		}
-		/*
-		else
-		{
-			USART2_ProcessData(&bufferUSART2dma[old_pos], DMA_USART2_BUFFER_SIZE - old_pos);
-
-			if (pos > 0)
-			{
-				USART2_ProcessData(&bufferUSART2dma[0], pos);
-			}
-		}
-		*/
 
 		else{
 			USART2_ProcessData(&bufferUSART2dma[old_pos], DMA_USART2_BUFFER_SIZE - old_pos);
-			//USART2_ProcessData(&bufferUSART2dma[old_pos]);
 			memset(bufferUSART2dma, 0, DMA_USART2_BUFFER_SIZE);
 			LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_6);
 			LL_DMA_ConfigAddresses(	DMA1, LL_DMA_CHANNEL_6,
@@ -286,24 +200,6 @@ void USART2_CheckDmaReception(void)
 		}
 
 	}
-/*
-	if (old_pos >= 20)
-	{
-		USART2_ProcessData(&bufferUSART2dma[old_pos], DMA_USART2_BUFFER_SIZE - old_pos);
-		memset(bufferUSART2dma, 0, DMA_USART2_BUFFER_SIZE);
-		LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_6);
-		LL_DMA_ConfigAddresses(	DMA1, LL_DMA_CHANNEL_6,
-											 	LL_USART_DMA_GetRegAddr(USART2, LL_USART_DMA_REG_DATA_RECEIVE),
-												(uint32_t)bufferUSART2dma,
-												LL_DMA_GetDataTransferDirection(DMA1, LL_DMA_CHANNEL_6));
-
-		LL_DMA_SetDataLength(DMA1, LL_DMA_CHANNEL_6, DMA_USART2_BUFFER_SIZE);
-		LL_DMA_EnableChannel(DMA1, LL_DMA_CHANNEL_6);
-		LL_USART_EnableDMAReq_RX(USART2);
-		old_pos = 0;
-	}
-	*/
-
 }
 
 
